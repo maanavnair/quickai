250603508715aed8f1c531560cc28192253929a9
Maanav Nair <maanavnair20042@gmail.com>
2025-08-20 15:50:37 +0530

    minor updates to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,24 +5,24 @@ const Sidebar = ({sidebar, setSidebar}) => {
 
     const {user} = useUser();
     const {signOut, openUserProfile} = useClerk();
-    console.log(user)
-    console.log(user?.fullName)
 
   return(
-    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between
-    items-center max-sm:absolute top-14 bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm: -translate-x-full'}
-    transition-all duration-300 ease-in-out`}>
-        <div className='my-7 w-full'>
-            <img  
-                src={user?.imageUrl}
-                alt='image'
-                className='w-14 rounded-full mx-auto'
-            />
-            <h1 className='mt-1 text-center'>
-                {user?.fullName}
-            </h1>
-        </div>
-    </div>
+    <div
+  className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between
+  items-center sm:relative sm:translate-x-0
+  max-sm:absolute top-14 bottom-0 left-0
+  ${sidebar ? "translate-x-0" : "-translate-x-full"}
+  transition-transform duration-300 ease-in-out`}
+>
+  <div className="my-7 w-full">
+    <img
+      src={user.imageUrl}
+      alt="profile"
+      className="w-20 h-20 rounded-full mx-auto"
+    />
+    <h1 className="mt-2 text-center font-medium">{user.fullName}</h1>
+  </div>
+</div>
   )
 }
 
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -8,31 +8,31 @@ import { useUser, SignIn } from '@clerk/clerk-react';
 const Layout = () => {
 
   const navigate = useNavigate();
-  const [sidebar, setSidebar] = useState(true);
+  const [sidebar, setSidebar] = useState(false);
   const {user} = useUser();
 
   return user ? (
     <div className='flex flex-col items-start justify-start h-screen'>
-        <nav>
-          <img 
-            src={assets.logo} 
-            alt="logo" 
-            onClick={() => navigate('/')} 
-            className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200 
-            cursor-pointer'
-          />
-          {
-            sidebar ? 
-            <X 
-              onClick={() => setSidebar(false)}
-              className='w-6 h-6 text-gray-600 sm:hidden' 
-            /> :
-            <Menu 
-              onClick={() => setSidebar(true)}
-              className='w-6 h-6 text-gray-600 sm:hidden' 
-            />
-          }
-        </nav>
+        <nav className="flex items-center justify-between px-4 min-h-14 border-b border-gray-200">
+  <img 
+    src={assets.logo} 
+    alt="logo" 
+    onClick={() => navigate('/')} 
+    className="h-10 cursor-pointer"
+  />
+  {
+    sidebar ? 
+    <X 
+      onClick={() => setSidebar(false)}
+      className="w-6 h-6 text-gray-600 sm:hidden cursor-pointer" 
+    /> :
+    <Menu 
+      onClick={() => setSidebar(true)}
+      className="w-6 h-6 text-gray-600 sm:hidden cursor-pointer" 
+    />
+  }
+</nav>
+
         <div className='flex-1 w-full flex h-[calc(100vh-64px)]'>
           <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
           <div className='flex-1 bg-[#F4F7FB]'>
