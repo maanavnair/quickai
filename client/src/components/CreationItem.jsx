d86988144164ee9827ac10b4ef5e7803c0af7bb5
Maanav Nair <maanavnair20042@gmail.com>
2025-08-21 18:11:08 +0530

    dashboard done

--- client/src/components/CreationItem.jsx
+++ client/src/components/CreationItem.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Markdown from 'react-markdown'
 
 const CreationItem = ({item}) => {
+
+    const [expanded, setExpanded] = useState(false);
+
   return (
-    <div className='p-4 max-w-5xl text-sm bg-white border border-gray-200 rounded-lg cursor-pointer'>
+    <div 
+        className='p-4 max-w-5xl text-sm bg-white border border-gray-200 rounded-lg cursor-pointer'
+        onClick={() => setExpanded(!expanded)}
+    >
         <div className='flex justify-between items-center gap-4'>
             <div>
                 <h2>
@@ -18,6 +25,29 @@ const CreationItem = ({item}) => {
                 {item.type}
             </button>
         </div>
+        {
+            expanded && (
+                <div>
+                    {item.type === 'image' ? (
+                        <div>
+                            <img 
+                                src={item.content}
+                                alt='image'
+                                className='mt-3 w-full max-w-md'
+                            />
+                        </div>
+                    ): (
+                        <div className='mt-3 h-full overflow-y-scroll text-sm text-shadow-slate-700'>
+                            <div className='reset-tw'>
+                                <Markdown>
+                                    {item.content}
+                                </Markdown>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )
+        }
     </div>
   )
 }
