3a91c18854981b9c11c6e177ef43e70f2743423b
Maanav Nair <maanavnair20042@gmail.com>
2025-08-25 18:50:52 +0530

    connected gemini api and added generate-article

--- server/server.js
+++ server/server.js
@@ -3,16 +3,19 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import { clerkMiddleware, requireAuth } from '@clerk/express';
+import aiRouter from './routes/aiRoutes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(clerkMiddleware);
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('Server is Live'));
 
 app.use(requireAuth());
+
+app.use('/api/ai', aiRouter);
 
 const PORT = process.env.PORT || 3000;
 
