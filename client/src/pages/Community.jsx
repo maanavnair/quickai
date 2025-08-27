307d4ea1ad0700f138f0c58d8878de5390d5af70
Maanav Nair <maanavnair20042@gmail.com>
2025-08-27 20:44:52 +0530

    added likes to community page

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -34,13 +34,33 @@ const Community = () => {
     setLoading(false);
   }
 
+  const imageLikeToggle = async (id) => {
+    try {
+      const { data } = await axios.post('/api/user/toggle-like-creation', {
+        id
+      }, {
+        headers: {Authorization: `Bearer ${await getToken()}`}
+      });
+
+      if(data.success){
+        toast.success(data.message);
+        await fetchCreations();
+      }
+      else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
   useEffect(() => {
     if(user){
       fetchCreations();
     }
   }, [user])
 
-  return (
+  return !loading ? (
     <div className='flex-1 h-full flex flex-col gap-4 p-6'>
         Creations
         <div className='bg-white h-full w-full rounded-xl overflow-y-scroll'>
@@ -64,15 +84,23 @@ const Community = () => {
                   <p>
                     {creation.likes.length}
                   </p>
-                  <Heart className={`min-w-5 h-5 hover:scale-110 cursor-pointer
+                  <Heart 
+                    onClick={() => imageLikeToggle(creation.id)}
+                    className={`min-w-5 h-5 hover:scale-110 cursor-pointer
                     ${creation.likes.includes(user.id) ? 'fill-red-500 text-red-600' : 
                       'text-white'
-                    }`} />
+                    }`} 
+                  />
                 </div>
               </div>
             </div>
           ))}
         </div>
+    </div>
+  ) : (
+    <div className='flex justify-center items-center h-full'>
+      <span className='w-10 h-10 my-1 rounded-full border-3 border-primary border-t-transparent animate-spin'>
+      </span>
     </div>
   )
 }
